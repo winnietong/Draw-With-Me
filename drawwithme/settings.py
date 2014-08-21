eba3f6023143a6c3fc1c38160bc8e0feb992602a
Winnie Tong <tong.winnie@gmail.com>
2014-08-20 20:49:58 -0700

    testing

--- drawwithme/settings.py
+++ drawwithme/settings.py
@@ -113,7 +113,7 @@ ALLOWED_HOSTS = ['*']
 # Static asset configuration
 import os
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
-STATIC_ROOT = 'staticfiles'
+# STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 
