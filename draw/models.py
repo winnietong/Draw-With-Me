e74d377b5923072c772b1495918ad2e6d42c254e
Rudy Mutter <rudy@yetihq.com>
2014-08-22 13:07:50 -0700

    Update models.py

--- draw/models.py
+++ draw/models.py
@@ -12,6 +12,9 @@ class User(AbstractUser):
 class Drawing(models.Model):
     local_path = models.CharField(max_length=120)
     title = models.CharField(max_length=120)
+    # related_names would make more sense as something like authored_drawings and followed_drawings
+    # you'd have a user object and would want to say get all of the drawings for this user where they're the author
+    # user.authored_drawings
     author = models.ManyToManyField(User, blank=True, null=True, related_name="author")
     follower = models.ManyToManyField(User, blank=True, null=True, related_name="follower")
 
